597fe8d0bfa3d3c600b41172aa449c568b291167
chenliujin <liujin.chen@qq.com>
2017-01-01 20:04:51 +0800

    update

--- z/model/z.php
+++ z/model/z.php
@@ -1,16 +1,6 @@
 <?php
-
 include_once('chenliujin/mysql/Model.class.php');
 include_once('chenliujin/mysql/Page.php');
+include_once('z/model/ZenCart.php');
 
 require('/data/z/conf/configure.php');
-
-if (empty(Model::$dbo)) {
-	Model::$host 	= DB_SERVER;
-	Model::$dbName 	= DB_DATABASE;
-	Model::$dbUser 	= DB_SERVER_USERNAME;
-	Model::$dbPwd   = DB_SERVER_PASSWORD;
-
-	Model::$dbo = new Model;
-}
-
